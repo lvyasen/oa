e4631b2b662a23f5de81ec75d2fad76a77646355
walker <18530893662@163.com>
2019-11-29 10:37:11 +0800

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -27,7 +27,7 @@
 
         // Step 3: Authorize the user.
 
-        var CLIENT_ID = '628874914398-8r7e6ceja032frp2vggr036qallokr06.apps.googleusercontent.com';
+        var CLIENT_ID = '628874914398-uqthhdqsb0sv06s1sork5j1uf4k69555.apps.googleusercontent.com';
 
         gapi.analytics.auth.authorize({
             container: 'auth-button',
